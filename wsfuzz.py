1139e34401c09bca57da53d5df60172e824cebdb
Alessio Giorgianni <alessio.giorgianni.1991@gmail.com>
2023-03-29 14:40:32 +0200

    main - cleaned usage manual

--- wsfuzz.py
+++ wsfuzz.py
@@ -1,16 +1,5 @@
 """
     Multithreaded websocket proxy to fuzz unknown endpoints, params or sqlmap proxy.
-
-    Path fuzzing:
-        - python3 wsfuzz.py path --ws "ws://ws.qreader.htb:5789/" --lhost 127.0.0.1 --lport 6060
-        - wfuzz -u http://127.0.0.1:6060/?param=FUZZ -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt --hc 404
-    Param fuzzing:
-        - python3 wsfuzz.py param --ws "ws://ws.qreader.htb:5789/endpoint" --lhost 127.0.0.1 --lport 6060
-        - wfuzz -u http://127.0.0.1:6060/?param=FUZZ -w /usr/share/seclists/Discovery/Web-Content/burp-parameter-names.txt --hc 404
-    SQLMap proxying:
-        - python3 wsfuzz.py sqlmap --ws "ws://ws.qreader.htb:5789/endpoint" --pname version --lhost 127.0.0.1 --lport 6060
-        - sqlmap -u http://127.0.0.1:6060/?param=test --level 5 --risk 3
-
     Coded By: nriver
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
